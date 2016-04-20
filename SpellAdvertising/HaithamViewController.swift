ae16369bab77dc5f22bded5c2dad1e4bbc62b58d
haitham tory <haithamtory@haithams-Mac.local>
2016-04-20 04:13:35 -0700

    comment added to haitham View

--- SpellAdvertising/HaithamViewController.swift
+++ SpellAdvertising/HaithamViewController.swift
@@ -14,6 +14,7 @@ class HaithamViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //comment soma comment
     }
 
     override func didReceiveMemoryWarning() {
